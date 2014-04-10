5181fdf496af183a45a7d1cbaeb169cce48ac6cc
Enric Mieza <emieza@xtec.cat>
2014-04-10 23:44:39 +0200

    poca cosa

--- django1/views.py
+++ django1/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 def home(request):
     return HttpResponse("""
 	Benvinguts a l'Almaixera.<br>
-	Realitza la teva <a href="/comandes">comanda</a>.
+	Realitza la teva <a href="comandes">comanda</a>.
 """)
     
     
