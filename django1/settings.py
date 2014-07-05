93b00a2bf928ee409106a674cb9fcb1b49e1b163
Enric Mieza <emieza@xtec.cat>
2014-07-05 09:22:15 +0200

    SITE_ID=1 better for default configurations

--- django1/settings.py
+++ django1/settings.py
@@ -43,7 +43,7 @@ TIME_ZONE = 'Europe/Madrid'
 # http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'ca-ES'
 
-SITE_ID = 3
+SITE_ID = 1
 
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
